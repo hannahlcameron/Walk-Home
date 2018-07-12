af8c0fab75ab0306c98f4cc0370b58365de437dc
Hannah Cameron <hannahiscoding@gmail.com>
2018-07-11 20:55:27 -0700

    added in modal component file

---
+++ walk-home/components/AddressDetail.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Modal, View, Text, StyleSheet} from "react-native";
+import PropTypes from 'prop-types';
+
+class AddressDetail extends React.Component {
+
+
+  render() {
+    return(
+      <Modal>
+        <View>
+          <Text></Text>
+        </View>
+      </Modal>
+    )
+  }
+
+}
+
+
+
+export default AddressDetail;
