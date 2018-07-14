b73b3c99fd049d60220a29d6f2fad5efb1006907
Hannah Cameron <hannahiscoding@gmail.com>
2018-07-13 17:06:15 -0700

    added log statements

--- walk-home/components/HouseList.js
+++ walk-home/components/HouseList.js
@@ -6,19 +6,19 @@ import { WSAPI_KEY } from 'react-native-dotenv';
 // import JSONData from './library/addresses.JSON';
 
 const house =
-      // JSON.parse(JSONData);
-      [{
-          "key": 1,
-          "streetNum": "3440",
-          "streetName": "Walnut",
-          "streetType": "Ave SW",
-          "city": "Seattle",
-          "state": "WA",
-          "latitude": 47.5718752,
-          "longitude": -122.3835876
-        }];
+// JSON.parse(JSONData);
+[{
+  "key": 1,
+  "streetNum": "3440",
+  "streetName": "Walnut",
+  "streetType": "Ave SW",
+  "city": "Seattle",
+  "state": "WA",
+  "latitude": 47.5718752,
+  "longitude": -122.3835876
+}];
 
-const wsURL = 'http://api.walkscore.com/score?format=json&address=3440%20Walnut%20Ave%20SW%20Seattle%20WA&lat=47.5718752&lon=-122.3835876&wsapikey='+ WSAPI_KEY
+const wsURL = 'http://api.walkscore.com/score?format=json&address=3440%20Walnut%20Ave%20SW%20Seattle%20WA&lat=47.5718752&lon=-122.3835876&wsapikey=' + WSAPI_KEY
 
 class HouseList extends React.Component {
   static propTypes = {
@@ -33,98 +33,128 @@ class HouseList extends React.Component {
   }
 
   componentDidMount() {
-            return fetch(wsURL)
-              .then((response) => response.json())
-              .then((responseJson) => {
-                console.log('in fetch ... .then');
-                console.log(responseJson.walkscore);
+    console.log('starting API CALL');
+    return fetch(wsURL)
+    .then((response) => {
+      response.json()
+      console.log('in 1st fetch .then');
+    })
+    .then((responseJson) => {
+      console.log('in 2nd fetch .then');
+      console.log(responseJson.walkscore);
 
-              })
-              .catch((error) => {
-                console.log('in fetch .. .catch');
-                console.log(error);
-              });
+    })
+    .catch((error) => {
+      console.log('in fetch .catch');
+      console.log(error);
+    });
   }
 
   render() {
-    return (
-      <Text>Cards go here!</Text>
-    )
+    let searchResults;
+
+    if (this.props.cityName === '') {
+      searchResults = (
+        <View>
+          <Text>Enter a city name to get your search going!</Text>
+        </View>)
+
+    } else if (this.props.cityName.toLowerCase() === 'seattle') {
+      searchResults =(
+        <ScrollView>
+          <Card
+            streetNum={house[0]["streetNum"]}
+            streetName={house[0]["streetName"]}
+            streetType={house[0]["streetType"]}
+            city={house[0]["city"]}
+            state={house[0]["state"]}
+          />
+        </ScrollView>
+      )
+    } else {
+      searchResults = (
+        <View>
+          <Text>Oops! No houses were found in {this.props.cityName}</Text>
+        </View>
+      )
+    }
+
+    return (searchResults)
+
   }
 
 }
 
-
-//   setCardInfo = () => {
-//     let searchResults =(
-//       <ScrollView>
-//         <Card
-//           streetNum={house[0]["streetNum"]}
-//           streetName={house[0]["streetName"]}
-//           streetType={house[0]["streetType"]}
-//           city={house[0]["city"]}
-//           state={house[0]["state"]}
-//           />
-//       </ScrollView> )
-//
-//       return searchResults
-//   }
-//
-//   render() {
-//
-//     let searchResults;
-//
-//     if (this.props.cityName === '') {
-//       searchResults = (
-//         <View>
-//           <Text>Enter a city name to get your search going!</Text>
-//         </View>)
-//
-//     } else if (this.props.cityName.toLowerCase() === 'seattle') {
-//         // took out for loop - throws error about symbol - need to find an alt way
-//         // of looping though the json - for Each?
-//         // for (let address of addressData) {
-//       // [
-//       //   addressData[0].ID,
-//       //   addressData[0].streetNum,
-//       //   addressData[0].streetName,
-//       //   addressData[0].streetType,
-//       //   addressData[0].city,
-//       //   addressData[0].state
-//       // ]
-//
-//         let wsURL = 'http://api.walkscore.com/score?format=json&address=3440%20Walnut%20Ave%20SW%20Seattle%20WA&lat=47.5718752&lon=-122.3835876&wsapikey='+ WSAPI_KEY
-//
-//         return fetch(wsURL)
-//           .then((response) => response.json())
-//           .then((responseJson) => {
-//             console.log('in fetch ... .then');
-//             console.log(responseJson.walkscore);
-//             // this.setCardInfo()
-//           })
-//           .catch((error) => {
-//             console.log('in fetch .. .catch');
-//             console.log(error);
-//           });
-//
-//
-//     } else {
-//       searchResults = (
-//         <View>
-//           <Text>Oops! No houses were found in {this.props.cityName}</Text>
-//         </View>
-//       )
-//     }
-//
-//     return (searchResults)
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   listContainer: {
-//     width: "100%",
-//     height: "50%"
-//   }
-// })
-
 export default HouseList;
+
+    //   setCardInfo = () => {
+    //     let searchResults =(
+    //       <ScrollView>
+    //         <Card
+    //           streetNum={house[0]["streetNum"]}
+    //           streetName={house[0]["streetName"]}
+    //           streetType={house[0]["streetType"]}
+    //           city={house[0]["city"]}
+    //           state={house[0]["state"]}
+    //           />
+    //       </ScrollView> )
+    //
+    //       return searchResults
+    //   }
+    //
+    //   render() {
+    //
+    //     let searchResults;
+    //
+    //     if (this.props.cityName === '') {
+    //       searchResults = (
+    //         <View>
+    //           <Text>Enter a city name to get your search going!</Text>
+    //         </View>)
+    //
+    //     } else if (this.props.cityName.toLowerCase() === 'seattle') {
+    //         // took out for loop - throws error about symbol - need to find an alt way
+    //         // of looping though the json - for Each?
+    //         // for (let address of addressData) {
+    //       // [
+    //       //   addressData[0].ID,
+    //       //   addressData[0].streetNum,
+    //       //   addressData[0].streetName,
+    //       //   addressData[0].streetType,
+    //       //   addressData[0].city,
+    //       //   addressData[0].state
+    //       // ]
+    //
+    //         let wsURL = 'http://api.walkscore.com/score?format=json&address=3440%20Walnut%20Ave%20SW%20Seattle%20WA&lat=47.5718752&lon=-122.3835876&wsapikey='+ WSAPI_KEY
+    //
+    //         return fetch(wsURL)
+    //           .then((response) => response.json())
+    //           .then((responseJson) => {
+    //             console.log('in fetch ... .then');
+    //             console.log(responseJson.walkscore);
+    //             // this.setCardInfo()
+    //           })
+    //           .catch((error) => {
+    //             console.log('in fetch .. .catch');
+    //             console.log(error);
+    //           });
+    //
+    //
+    //     } else {
+    //       searchResults = (
+    //         <View>
+    //           <Text>Oops! No houses were found in {this.props.cityName}</Text>
+    //         </View>
+    //       )
+    //     }
+    //
+    //     return (searchResults)
+    //   }
+    // }
+
+    // const styles = StyleSheet.create({
+    //   listContainer: {
+    //     width: "100%",
+    //     height: "50%"
+    //   }
+    // })
